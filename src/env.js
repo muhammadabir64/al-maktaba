03fbc3317509deca4482fd966c288f7e47d61fe6
Muhammad ABir <muhammadabir99@gmail.com>
2023-10-06 19:29:37 +0600

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 export const DOMAIN = 'https://al-maktaba.netlify.app';
-export const OLS_SITE = 'http://localhost:3000';
+export const OLS_SITE = 'https://muhammadabir64.github.io/ummah-path';
 export const GITHUB_REPO = 'https://github.com/muhammadabir64/al-maktaba';
